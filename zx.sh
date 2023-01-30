0fd29cc700b9ff2e8c5374d7ff627d1351fa7ebb
Abhijeet Nikam <35236714+abhijeetnikam1995@users.noreply.github.com>
2023-01-30 20:11:56 +0530

    test

---
+++ zx.sh
@@ -0,0 +1,5 @@
+private_subnets_id = "subnet-0eb68349b8644acb2,subnet-01babbbebd9748916"
+public_subnets_id = "subnet-0fd8e9350c217eeba,subnet-099c5ea03fe483e59"
+ssh_sg_id = "sg-01f7f510ace0194e9"
+vpc_id = "vpc-039079ccf56d80036"
+
